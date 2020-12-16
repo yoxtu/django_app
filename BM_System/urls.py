acec6e5090307e6e1347f1c92261e77e0de24c60
yoxtu <yoxtu11231123@gmail.com>
2020-12-16 10:09:39 +0900

    bm_system ver 0.01

--- BM_System/urls.py
+++ BM_System/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('stock',views.stock,name='stock'),
+    path('lending',views.lending,name='lending'),
+    path('login',views.login,name='login'),
+    path('contact',views.contact,name='contact')
 ]
--- BM_System/views.py
+++ BM_System/views.py
@@ -9,3 +9,31 @@ def index(request):
         'data':'data',
     }
     return render(request, 'BM_System/index.html',params)
+
+def stock(request):
+    params = {
+        'title': 'BM_System',
+        'data':'data',
+    }
+    return render(request, 'BM_System/index.html',params)
+
+def lending(request):
+    params = {
+        'title': 'BM_System',
+        'data':'data',
+    }
+    return render(request, 'BM_System/index.html',params)
+
+def login(request):
+    params = {
+        'title': 'BM_System',
+        'data':'data',
+    }
+    return render(request, 'BM_System/index.html',params)
+
+def contact(request):
+    params = {
+        'title': 'BM_System',
+        'data':'data',
+    }
+    return render(request, 'BM_System/index.html',params)
