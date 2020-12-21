7865dc3054e25d91db034aeed69901c65b205192
yoxtu <yoxtu11231123@gmail.com>
2020-12-22 01:35:23 +0900

    bm_system ver 0.01

--- BM_System/views.py
+++ BM_System/views.py
@@ -13,27 +13,23 @@ def index(request):
 def stock(request):
     params = {
         'title': 'BM_System',
-        'data':'data',
     }
-    return render(request, 'BM_System/index.html',params)
+    return render(request, 'BM_System/stock.html',params)
 
 def lending(request):
     params = {
         'title': 'BM_System',
-        'data':'data',
     }
-    return render(request, 'BM_System/index.html',params)
+    return render(request, 'BM_System/lending.html',params)
 
 def login(request):
     params = {
         'title': 'BM_System',
-        'data':'data',
     }
-    return render(request, 'BM_System/index.html',params)
+    return render(request, 'BM_System/login.html',params)
 
 def contact(request):
     params = {
         'title': 'BM_System',
-        'data':'data',
     }
-    return render(request, 'BM_System/index.html',params)
+    return render(request, 'BM_System/contact.html',params)
